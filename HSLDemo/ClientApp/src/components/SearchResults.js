d07e6858cd3d6453e07956c6123ac2643e892e22
Petri Tuononen <petri.tuononen@vartas.fi>
2021-05-04 17:41:21 +0300

    Added info text so that user knows to click search result

--- HSLDemo/ClientApp/src/components/SearchResults.js
+++ HSLDemo/ClientApp/src/components/SearchResults.js
@@ -11,13 +11,14 @@ export class SearchResults extends Component {
                         (this.props.searchResults.length > 0 &&
                             <>
                             <Card>
-                                <CardBody>
-                                    {this.props.searchResults.map((result, idx) =>
-                                        <Button block key={'searchResultBtn-' + idx} outline={this.props.selectedResult !== result}
-                                            color="secondary" className="searchResultButton" onClick={() => this.props.handleSelectResult(result)}>
-                                            {result.label}
-                                        </Button>
-                                    )}
+                            <CardBody>
+                                <p>Best matches: (Click to highlight)</p>
+                                {this.props.searchResults.map((result, idx) =>
+                                    <Button block key={'searchResultBtn-' + idx} outline={this.props.selectedResult !== result}
+                                        color="secondary" className="searchResultButton" onClick={() => this.props.handleSelectResult(result)}>
+                                        {result.label}
+                                    </Button>
+                                )}
                                 </CardBody>
                                 <CardFooter>
                                     <Button id="hide-results" outline color="secondary" size="sm" onClick={() => this.props.handleHideResults()}>Hide results</Button>
